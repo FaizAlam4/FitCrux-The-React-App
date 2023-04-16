07ae96dc16ad7c928b5fcfc71103a56eca4e75fd
Faiz <faizalam4e@gmail.com>
2023-04-16 22:51:59 +0530

    First commit

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Heading from './Heading';
+import Footer from './Footer';
+import Home from './Home';
+
+
+function App() {
+  return (
+    <div className="App">
+   <Heading/>
+   <Home/>
+   <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Heading.js
@@ -0,0 +1,16 @@
+import './Heading.css'
+
+
+
+function Heading(){
+    return <header className="head">
+        <div className="container">
+            <div className="cont">Home</div>
+            <div className="cont">About</div>
+            <div className="cont">Login</div>
+            <div className="cont">Help</div>
+        </div>
+    </header>
+}
+
+export default Heading;
