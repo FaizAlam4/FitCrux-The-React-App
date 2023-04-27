11399f2b239f462b958d81beb98e13acc5737fab
Faiz <faizalam4e@gmail.com>
2023-04-27 10:08:55 +0530

    About page done

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import './About.css';
-import Abtlogo from '../assets/abtlogo.svg'
+import Abtlogo from '../assets/abtpic12.png'
 
 function About(){
 
-return <div id="abt"> 
-<img id='abtimg' src={Abtlogo} alt="" />
-<div>
-    
+return <div className='ac' id="abt"> 
+ <div id='ac1'  style={{border:"none"}}> <img id='abtimg' src={Abtlogo} alt="" /></div>  
+<div className='ac' id='ac2'>
+    <p>Say goodbye to excuses
+and hello to positive uses
+of your time and energy as
+our website's the remedy for that you just have to
+join our fitness website today to
+find your motivation and stay
+with workouts, tips, and guidance too
+your goals are achievable, it's true.</p>
+</div>
+<div className='ac' id='ac3'>
+    <ul>Why FitnessCrux ?
+        <hr />
+    <li>Free to use</li>
+    <li>Home Workout plan</li>
+    <li>Measure progress</li>
+    <li>Schedule tasks</li>
+    <li>Diet planner</li>
+    </ul>
 </div>
 </div> 
 }
